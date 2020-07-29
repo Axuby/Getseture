50efd5a7b62bdeca9ceb19d995133e5520382920
Axuby <azubuinesamuel@gmail.com>
2020-07-29 15:35:37 +0100

    created react app and added hompage and the sass styles

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Homepage from "./components/homepage.components"
+import './App.css';
+
+function App() {
+  return (
+    <div> Hello world
+      <Homepage/>
+    </div>
+  );
+} 
+
+export default App;
